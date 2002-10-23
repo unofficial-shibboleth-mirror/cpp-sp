e58b2c4270932b902b8fd3fed12e8510345fa19d
Derek Atkins <derek@ihtfp.com>
2002-10-23 03:15:44 +0000

    move scott's shibrpc.h fix to shibrpc.x so it doesn't get lost. push tli onto the client socket before trying RPC (Solaris fix)

--- shib-target/shib-rpcutil.c
+++ shib-target/shib-rpcutil.c
@@ -8,6 +8,14 @@
 
 #include <sys/socket.h>
 
+#include "config.h"
+
+#ifdef NEED_RPC_TLI
+#ifdef HAVE_STROPTS_H
+#include <stropts.h>
+#endif
+#endif
+
 #include "shib-target.h"
 
 CLIENT *
@@ -17,6 +25,18 @@ shibrpc_client_create (ShibSocket sock, u_long program, u_long version)
 
   memset (&sin, 0, sizeof (sin));
   sin.sin_port = 1;
+
+#ifdef NEED_RPC_TLI
+  /*
+   * there's an undocumented restriction that the fd you pass in
+   * needs to support the TLI operations.
+   */
+  if (ioctl (sock, I_PUSH, "timod") < 0) {
+    perror("I_PUSH");
+    close (sock);
+    return NULL;
+  }
+#endif
 
   return clnttcp_create (&sin, program, version, &sock, 0, 0);
 }
