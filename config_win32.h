e300d94668049afb492a40f9f4b5c73cbb586738
Scott Cantor <cantor.2@osu.edu>
2003-06-28 04:40:59 +0000

    Added strtok define.

--- config_win32.h
+++ config_win32.h
@@ -64,6 +64,9 @@
 /* Define to 1 if you have the `strstr' function. */
 #define HAVE_STRSTR 1
 
+/* Define to 1 if you have the `strtok_r' function. */
+/* #undef HAVE_STRTOK_R */
+
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
 
@@ -83,13 +86,13 @@
 #define PACKAGE_NAME "shibboleth"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "shibboleth 1.0"
+#define PACKAGE_STRING "shibboleth 1.0.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "shibboleth"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0"
+#define PACKAGE_VERSION "1.0.1"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -102,7 +105,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "1.0"
+#define VERSION "1.0.1"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
