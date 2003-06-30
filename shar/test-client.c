1b8fe814fa5d44c31734ac74881b6ea4367997ca
Scott Cantor <cantor.2@osu.edu>
2003-06-30 04:44:43 +0000

    Added winsock calls.

--- shar/test-client.c
+++ shar/test-client.c
@@ -1,32 +1,47 @@
-#include <unistd.h>
 #include <stdio.h>
-
 #include <shib-target/shib-target.h>
 
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-  int sock, res;
+  ShibSocket sock;
+  int res,start;
   CLIENT *clnt;
   enum clnt_stat clnt_stat;
 
-  if (shib_sock_create (&sock) != 0)
+#if defined(WIN32) && !defined(__GNUC__) || defined(__MINGW32__)
+  WORD wVersionRequested;  
+  WSADATA wsaData; 
+  int err; 
+  wVersionRequested = MAKEWORD(1, 0); 
+    
+  err = WSAStartup(wVersionRequested, &wsaData);
+#endif
+
+  if ((res=shib_sock_create(&sock)) != 0)
+  {
+    printf("shib_sock_create failed: %d\n",res);
     return -1;
+  }
 
-  if (shib_sock_connect (sock, SHIB_SHAR_SOCKET) != 0)
+  if ((res=shib_sock_connect(sock, SHIB_SHAR_SOCKET)) != 0)
+  {
+    printf("shib_sock_connect failed: %d\n",res);
     return -2;
+  }
 
-  clnt = shibrpc_client_create (sock, SHIBRPC_PROG, SHIBRPC_VERS_1);
+  clnt = shibrpc_client_create(sock, SHIBRPC_PROG, SHIBRPC_VERS_1);
   if (!clnt) {
     clnt_pcreateerror ("shibrpc_client_create");
+    printf("shibrpc_client_create failed\n");
     return -3;
   }
 
-  res = 0;
-  clnt_stat = shibrpc_ping_1 (&sock, &res, clnt);
+  res = start = 0;
+  clnt_stat = shibrpc_ping_1 (&start, &res, clnt);
 
   if (clnt_stat != RPC_SUCCESS) {
     clnt_perror (clnt, "rpc");
+    printf("RPC error: %d, %d\n",clnt_stat,res);
     return -4;
   }
 
