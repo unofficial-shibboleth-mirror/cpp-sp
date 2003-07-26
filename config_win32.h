ce165c5ca5fb49de07bcea53007a6f05318c9e41
Scott Cantor <cantor.2@osu.edu>
2003-07-26 20:11:34 +0000

    Updated package version.

--- config_win32.h
+++ config_win32.h
@@ -86,13 +86,13 @@
 #define PACKAGE_NAME "shibboleth"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "shibboleth 1.0.1"
+#define PACKAGE_STRING "shibboleth 1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "shibboleth"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.1"
+#define PACKAGE_VERSION "1.1"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -108,7 +108,7 @@
 #define USE_OUR_ONCRPC 1
 
 /* Version number of package */
-#define VERSION "1.0.1"
+#define VERSION "1.1"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
