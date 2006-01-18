a44d5550281a41a92d6438e0398b5a508f5ddb07
Scott Cantor <cantor.2@osu.edu>
2006-01-18 04:26:50 +0000

    Reversed checkName in subjectAltName loop.

--- shib/ShibbolethTrust.cpp
+++ shib/ShibbolethTrust.cpp
@@ -341,7 +341,7 @@ bool ShibbolethTrust::validate(void* certEE, const Iterator<void*>& certChain, c
                 STACK_OF(GENERAL_NAME)* altnames=(STACK_OF(GENERAL_NAME)*)X509_get_ext_d2i(x, NID_subject_alt_name, NULL, NULL);
                 if (altnames) {
                     int numalts = sk_GENERAL_NAME_num(altnames);
-                    for (int an=0; !checkName && an<numalts; an++) {
+                    for (int an=0; checkName && an<numalts; an++) {
                         const GENERAL_NAME* check = sk_GENERAL_NAME_value(altnames, an);
                         if (check->type==GEN_DNS || check->type==GEN_URI) {
                             const char* altptr = (char*)ASN1_STRING_data(check->d.ia5);
