d7f1aa64bd997e3fe59ef43c4f6496a6aa77c51c
Scott Cantor <cantor.2@osu.edu>
2007-05-31 21:52:49 +0000

    Unix installation fixes

--- shibsp/impl/XMLServiceProvider.cpp
+++ shibsp/impl/XMLServiceProvider.cpp
@@ -445,8 +445,8 @@ XMLApplication::XMLApplication(
         SPConfig& conf=SPConfig::getConfig();
 #ifndef SHIBSP_LITE
         SAMLConfig& samlConf=SAMLConfig::getConfig();
-#endif
         XMLToolingConfig& xmlConf=XMLToolingConfig::getConfig();
+#endif
 
         // This used to be an actual hash, but now it's just a hex-encode to avoid xmlsec.
         static char DIGITS[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
