bdd257beec7ed612e19991d69d95e91f1cb76946
Scott Cantor <cantor.2@osu.edu>
2011-06-16 13:38:33 +0000

    Fix bug link

--- config_win32.h
+++ config_win32.h
@@ -126,7 +126,7 @@
 #define PACKAGE "shibboleth"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://bugs.internet2.edu/"
+#define PACKAGE_BUGREPORT "https://issues.shibboleth.net/"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "shibboleth"
