de97bb285010eaf5daf776c1deb92ebd67d62926
Rod Widdowson <rdw@steadingsoftware.com>
2012-05-31 12:23:02 +0000

    Handle Updates a bit better
    - Have a separate dialog for updates (allowing us to refactor the current dialog into a separate file)
    - Save all user supplied parameters (in case we need them later)
    - Set up parameters from these as appropriate for the update case.
    - Remove some old cruft from no-longer needed Dialogs
    
    We still have some work to do because an update will sense shibd running and we are about to stop it.

--- msi/WiX/Compile.bat
+++ msi/WiX/Compile.bat
@@ -16,12 +16,18 @@ candle ShibbolethSP-registry-x86.wxs
 wixcop -indent:2 ShibbolethSP-gui.wxs
 candle ShibbolethSP-gui.wxs
 
+wixcop -indent:2 ShibbolethSP-update-dlg.wxs
+candle ShibbolethSP-update-dlg.wxs
+
+wixcop -indent:2 ShibbolethSP-install-dlg.wxs
+candle ShibbolethSP-install-dlg.wxs
+
 wixcop -indent:2 ShibbolethSP-main-x64.wxs
 candle ShibbolethSP-main-x64.wxs
-light -sw1055 -sice:ICE82 -o ShibbolethSP-main-x64.msi ShibbolethSP-main-x64.wixobj ShibbolethSP-exe-x64.wixobj ShibbolethSP-registry-x64.wixobj  ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj ShibbolethSP-gui.wixobj -ext WixUtilExtension.dll -ext WixUIExtension -ext WixIISExtension
+light -sw1055 -sice:ICE82 -o ShibbolethSP-main-x64.msi ShibbolethSP-main-x64.wixobj ShibbolethSP-exe-x64.wixobj ShibbolethSP-registry-x64.wixobj  ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj ShibbolethSP-gui.wixobj ShibbolethSP-update-dlg.wixobj ShibbolethSP-install-dlg.wixobj -ext WixUtilExtension.dll -ext WixUIExtension -ext WixIISExtension
 
 wixcop -indent:2 ShibbolethSP-main-x86.wxs
 candle ShibbolethSP-main-x86.wxs
-light -sw1055 -sice:ICE82 -o ShibbolethSP-main-x86.msi ShibbolethSP-main-x86.wixobj ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj  ShibbolethSP-gui.wixobj -ext WixUtilExtension.dll -ext WixUIExtension  -ext WixIISExtension
+light -sw1055 -sice:ICE82 -o ShibbolethSP-main-x86.msi ShibbolethSP-main-x86.wixobj ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj  ShibbolethSP-gui.wixobj ShibbolethSP-update-dlg.wixobj ShibbolethSP-install-dlg.wixobj -ext WixUtilExtension.dll -ext WixUIExtension  -ext WixIISExtension
 
 
