abf0448140f2bcf73d0eabdc2fe443e500697d03
Scott Cantor <cantor.2@osu.edu>
2012-06-20 20:53:27 +0000

    Add WiX to path

--- msi/WiX/Compile.bat
+++ msi/WiX/Compile.bat
@@ -1,3 +1,5 @@
+PATH=%PATH%;"C:\Program Files (x86)\Windows Installer XML v3.5\bin"
+
 wixcop -indent:2 ShibbolethSP-exe-x64.wxs
 candle ShibbolethSP-exe-x64.wxs
 
@@ -24,8 +26,8 @@ candle ShibbolethSP-install-dlg.wxs
 
 wixcop -indent:2 ShibbolethSP-main-x64.wxs
 candle ShibbolethSP-main-x64.wxs
-light -sw1055 -sice:ICE82 -o shibboleth-sp-2.5.0-win64.msi ShibbolethSP-main-x64.wixobj ShibbolethSP-exe-x64.wixobj ShibbolethSP-registry-x64.wixobj  ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj ShibbolethSP-gui.wixobj ShibbolethSP-update-dlg.wixobj ShibbolethSP-install-dlg.wixobj -ext WixUtilExtension.dll -ext WixUIExtension -ext WixIISExtension
+light -sw1055 -sice:ICE82 -o shibboleth-sp-2.5.0b1-win64.msi ShibbolethSP-main-x64.wixobj ShibbolethSP-exe-x64.wixobj ShibbolethSP-registry-x64.wixobj  ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj ShibbolethSP-gui.wixobj ShibbolethSP-update-dlg.wixobj ShibbolethSP-install-dlg.wixobj -ext WixUtilExtension.dll -ext WixUIExtension -ext WixIISExtension
 
 wixcop -indent:2 ShibbolethSP-main-x86.wxs
 candle ShibbolethSP-main-x86.wxs
-light -sw1055 -sice:ICE82 -o shibboleth-sp-2.5.0-win32.msi ShibbolethSP-main-x86.wixobj ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj  ShibbolethSP-gui.wixobj ShibbolethSP-update-dlg.wixobj ShibbolethSP-install-dlg.wixobj -ext WixUtilExtension.dll -ext WixUIExtension  -ext WixIISExtension
+light -sw1055 -sice:ICE82 -o shibboleth-sp-2.5.0b1-win32.msi ShibbolethSP-main-x86.wixobj ShibbolethSP-noarch.wixobj ShibbolethSP-exe-x86.wixobj ShibbolethSP-registry-x86.wixobj  ShibbolethSP-gui.wixobj ShibbolethSP-update-dlg.wixobj ShibbolethSP-install-dlg.wixobj -ext WixUtilExtension.dll -ext WixUIExtension  -ext WixIISExtension
