83ea55371f0c0896d207212cf1d8ae1d91f708f3
Scott Cantor <cantor.2@osu.edu>
2012-07-09 14:22:35 +0000

    Add warning about IIS/Apache paths.

--- configs/SetService32.bat
+++ configs/SetService32.bat
@@ -1,5 +1,8 @@
 @echo off
 setlocal
+
 sc stop shibd_default
 sc config shibd_default binPath= "@-INSTALLDIR-@\sbin\shibd.exe -stdout \"@-INSTALLDIR-@\var\log\shibboleth\stdout.log\" -stderr \"@-INSTALLDIR-@\var\log\shibboleth\stderr.log\""
 sc start shibd_default
+
+echo NOTE: You will need to manually adjust your IIS or Apache configuration to complete a switch to 32-bit use.
--- configs/SetService64.bat
+++ configs/SetService64.bat
@@ -1,5 +1,8 @@
 @echo off
 setlocal
+
 sc stop shibd_default
 sc config shibd_default binPath= "@-INSTALLDIR-@\sbin64\shibd.exe -stdout \"@-INSTALLDIR-@\var\log\shibboleth\stdout.log\" -stderr \"@-INSTALLDIR-@\var\log\shibboleth\stderr.log\""
 sc start shibd_default
+
+echo NOTE: You will need to manually adjust your IIS or Apache configuration to complete a switch to 64-bit use.
