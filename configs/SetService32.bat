be14df6304c5f3e589bf2f23f4b493d0cc131586
Rod Widdowson <rdw@steadingsoftware.com>
2012-08-06 14:12:05 +0000

    Add a 3 second pause between changing the path and restarting the service

--- configs/SetService32.bat
+++ configs/SetService32.bat
@@ -3,6 +3,7 @@ setlocal
 
 sc stop shibd_default
 sc config shibd_default binPath= "@-INSTALLDIR-@\sbin\shibd.exe -stdout \"@-INSTALLDIR-@\var\log\shibboleth\stdout.log\" -stderr \"@-INSTALLDIR-@\var\log\shibboleth\stderr.log\""
+ping 1.1.1.1 -n 1 -w 3000 > nul
 sc start shibd_default
 
 echo NOTE: You will need to manually adjust your IIS or Apache configuration to complete a switch to 32-bit use.
