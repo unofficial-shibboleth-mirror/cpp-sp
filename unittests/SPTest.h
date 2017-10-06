d8717c7b98708008c83d1ae66d72155193c515af
Rod Widdowson <rdw@steadingsoftware.com>
2017-10-06 13:49:57 +0100

    SSPCPP-756 Add logging setup to SP test framework.
    https://issues.shibboleth.net/jira/browse/SSPCPP-756

--- unittests/SPTest.h
+++ unittests/SPTest.h
@@ -1,28 +1,41 @@
 #include <fstream>
 #include "BaseTestCase.h"
+#include <shibsp/SPConfig.h>
 #include <cxxtest/GlobalFixture.h>
-#include <xmltooling/XMLToolingConfig.h>
-#include <xmltooling/util/ParserPool.h>
+#include <shibsp\metadata\MetadataExt.h>
+#include <shibsp\ServiceProvider.h>
 
-using namespace xmltooling;
-using namespace xercesc;
-using namespace std;
+using namespace shibsp;
 
-
-string data_path = "../xmltoolingtest/data/";
+std::string data_path = "unittests/data/";
+std::string config_path = "unittests/config";
 
 class ToolingFixture : public CxxTest::GlobalFixture
 {
 public:
     bool setUpWorld() {
+        SPConfig& conf = SPConfig::getConfig();
+        // Initialize the SP library.
+        conf.setFeatures(
+            SPConfig::Metadata |
+            SPConfig::Logging);
+
+        if (!conf.init(nullptr, config_path.c_str())) {
+            fprintf(stderr, "configuration is invalid, see console for specific problems\n");
+            return false;
+        }
+
+        registerMetadataExtClasses();
+
         return true;
     }
     bool tearDownWorld() {
+        SPConfig::getConfig().term();
         return true;
     }
-    //bool setUp() { printf( "</test>" ); return true; }
-    //bool tearDown() { printf( "</test>" ); return true; }
 };
+
+static ToolingFixture globalFixture;
 
 
 class GlobalTest : public CxxTest::TestSuite
