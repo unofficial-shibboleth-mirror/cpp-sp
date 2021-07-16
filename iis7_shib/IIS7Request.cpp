969d25df2d9112d5091d0253896000f08869ced2
Scott Cantor <cantor.2@osu.edu>
2021-07-16 09:21:57 -0400

    SSPCPP-938 - IIS Module not setting Cache-Control
    https://issues.shibboleth.net/jira/browse/SSPCPP-938

--- iis7_shib/IIS7Request.cpp
+++ iis7_shib/IIS7Request.cpp
@@ -428,6 +428,8 @@ void IIS7Request::setResponseHeader(const char* name, const char* value, bool re
 long IIS7Request::sendRedirect(const char* url)
 {
     HTTPResponse::sendRedirect(url);
+    setResponseHeader("Expires", "Wed, 01 Jan 1997 12:00:00 GMT", true);
+    setResponseHeader("Cache-Control", "private,no-store,no-cache,max-age=0", true);
     HRESULT hr = m_response->Redirect(url);
     if (FAILED(hr)) {
         logFatal("Redirect", hr);
