942c155eb06a7d15de671c07df57df0aa003c90a
Rod Widdowson <rdw@steadingsoftware.com>
2024-12-06 17:00:12 +0000

    More warning supression to DiscoveryFeed.cpp

--- shibsp/handler/impl/DiscoveryFeed.cpp
+++ shibsp/handler/impl/DiscoveryFeed.cpp
@@ -48,7 +48,7 @@ namespace shibsp {
 
 #if defined (_MSC_VER)
     #pragma warning( push )
-    #pragma warning( disable : 4250 )
+    #pragma warning( disable : 4250 4251)
 #endif
 
     class SHIBSP_DLLLOCAL Blocker : public DOMNodeFilter
