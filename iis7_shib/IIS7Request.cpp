8ca1bfbb56c198ff70f03931c84444a4141b390b
Scott Cantor <cantor.2@osu.edu>
2025-07-23 20:07:27 -0400

    Add missing constants, update some headers.

--- iis7_shib/IIS7Request.cpp
+++ iis7_shib/IIS7Request.cpp
@@ -1,22 +1,16 @@
 /**
-* Licensed to the University Corporation for Advanced Internet
-* Development, Inc. (UCAID) under one or more contributor license
-* agreements. See the NOTICE file distributed with this work for
-* additional information regarding copyright ownership.
-*
-* UCAID licenses this file to you under the Apache License,
-* Version 2.0 (the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the
-* License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-* either express or implied. See the License for the specific
-* language governing permissions and limitations under the License.
-*/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "IIS7_shib.hpp"
 
@@ -65,7 +59,7 @@ IIS7Request::IIS7Request(IHttpContext *pHttpContext, IHttpEventProvider *pEventP
     }
 
     // Check for header/variable settings in RequestMap and only if absent fall back to Site info.
-    const PropertySet* mapopedProperties = getRequestSettings().first;
+    const PropertySet* mappedProperties = getRequestSettings().first;
     if (mappedProperties->hasProperty(RequestMapper::USE_HEADERS_PROP_NAME)) {
         m_useHeaders = mappedProperties->getBool(
             RequestMapper::USE_HEADERS_PROP_NAME, RequestMapper::USE_HEADERS_PROP_DEFAULT);
