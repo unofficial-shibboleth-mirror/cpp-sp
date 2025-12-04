1e711bc1fce8f50a833c245584ae19e67547f0ce
Scott Cantor <cantor.2@osu.edu>
2025-12-04 17:05:46 -0500

    Backstop unwrapped redirects with sanity checks.

--- shibsp/handler/impl/AbstractHandler.cpp
+++ shibsp/handler/impl/AbstractHandler.cpp
@@ -180,7 +180,10 @@ pair<bool,long> AbstractHandler::unwrapResponse(SPRequest& request, DDF& wrapped
 
     h = http["redirect"];
     if (h.isstring()) {
-        return make_pair(true, request.sendRedirect(h.string()));
+        request.limitRedirect(h.string());
+        string dest(h.string());
+        request.absolutize(dest);
+        return make_pair(true, request.sendRedirect(dest.c_str()));
     }
 
     h = http["response"];
