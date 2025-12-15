39107be795a279e163f42281bbd5a8cdce47baff
Scott Cantor <cantor.2@osu.edu>
2025-12-15 08:31:47 -0500

    Bump version.

--- config_win32.h
+++ config_win32.h
@@ -121,13 +121,13 @@
 #define PACKAGE_NAME "shibboleth"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "shibboleth 3.5.1"
+#define PACKAGE_STRING "shibboleth 3.5.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "shibboleth-sp"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.5.1"
+#define PACKAGE_VERSION "3.5.2"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -140,7 +140,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "3.5.1"
+#define VERSION "3.5.2"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
