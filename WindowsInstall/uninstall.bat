08379c979e0671ac98272cf9c1efa48ded2d2591
Rod Widdowson <rdw@steadingsoftware.com>
2026-04-03 16:47:31 +0100

    CPPSP-53 Replace Windows Installer with Bat and C program (for registry operations)
    https://shibboleth.atlassian.net/browse/CPPSP-53
    
    More debugging

--- WindowsInstall/uninstall.bat
+++ WindowsInstall/uninstall.bat
@@ -5,20 +5,26 @@ REM
 
 Rem Goto root of install - we are running from %targetDir%\dist-4.x.7\dist-bin\
 
-cd %dp0..\.
+cd /d %1%
+set TargetDir=%cd%
+
+if not exist "%targetdir%\bin\shibboleth-sp" (
+   echo "No installation found %targetdir%
+   exit /b
+)
+
 if exist %SYSTEMROOT%\System32\INETSRV\appcmd.exe (
     echo Uninstalling ShibAgent module from IIS
     %SYSTEMROOT%\System32\INETSRV\appcmd.exe delete module ShibAgent
     %SYSTEMROOT%\System32\INETSRV\appcmd.exe uninstall module ShibAgent
 )
-set base=%cd%
 
-echo Removing %base%\bin\shibboleth-sp\
-rd /s /q "%base%\bin\shibboleth-sp"
+echo Removing %TargetDir%\bin\shibboleth-sp\
+rd /s /q "%TargetDir%\bin\shibboleth-sp"
 
-echo Removing %base%\lib\shibboleth-sp\
-rd /s /q "%base%\lib\shibboleth-sp"
+echo Removing %TargetDir%\lib\shibboleth-sp\
+rd /s /q "%TargetDir%\lib\shibboleth-sp"
 
-echo Not touching %base%\etc\shibboleth-sp\
+echo Not touching %TargetDir%\etc\shibboleth-sp\
 
 echo "just add code to remove [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{D9DA52E3-F96E-4C84-B153-C3B17C34F730}]" and reg keys
--- WindowsInstall/update.bat
+++ WindowsInstall/update.bat
@@ -4,15 +4,19 @@ setlocal
 
 rem fail if parameters is not a distribution
 
-if not exist "%1%\dist\dist-bin\doupdate.bat" (
+rem find kit root
+cd %1%
+set kit_root=%CD%
+
+if not exist "dist\dist-bin\doupdate.bat" (
    echo "%1% is not a Shibboleth agent distribution"
    exit /b
 )
 
 rem find root of install
-cd %~dp0..
+cd %~dp0..\..
 set install_path=%cd%
 
-cd "%1%\dist\dist-bin\"
+cd "%kit_root%\dist\dist-bin\"
 cmd /c doupdate.bat "%install_path%"
 
